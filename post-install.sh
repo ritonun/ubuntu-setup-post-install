6c2e6ea1f0c5e63e07b242197bd4ecbf8363e793
ritonun <73615644+ritonun@users.noreply.github.com>
2025-09-05 12:33:37 +0200

    check the script is run with sudo; exit otherwise

--- post-install.sh
+++ post-install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # Ensure the computer is properly setup for the scripts
-. scripts/first_check.sh
+if . scripts/first_check.sh; then
+    log INFO "Prerequisites are completed. Continuing with scripts install."
+else
+    log ERROR "Ending the post-install script."
+    exit 1
+fi
--- scripts/first_check.sh
+++ scripts/first_check.sh
@@ -3,7 +3,17 @@
 # load library function and var
 . scripts/lib.sh
 
+check_script_run_with_sudo() {
+    # check the script is run with the sudo command
+    # if the user is root, $EUID should be 0
+    if ! [ $(id -u) = 0 ]; then
+        log ERROR "The script need to be run as root."
+        exit 1
+    fi
+}
+
 check_internet() {
+    # check internet connection by pinging cloudflare dns (1.1.1.1) and google dns (8.8.8.8)
     if ping -c 1 -w 1 8.8.8.8 &> /dev/null || ping -c 1 -w 1 1.1.1.1 &> /dev/null; then
         log INFO "Internet check: OK"
     else
@@ -12,4 +22,5 @@ check_internet() {
     fi
 }
 
+check_script_run_with_sudo
 check_internet
