1472901fa79daf717205e73aad5deb4be189fe1b
ritonun <73615644+ritonun@users.noreply.github.com>
2025-09-05 12:53:43 +0200

    rename first_check.sh to 00-precheck.sh

--- post-install.sh
+++ post-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ensure the computer is properly setup for the scripts
-if . scripts/first_check.sh; then
+if . scripts/00-precheck.sh; then
     log INFO "Prerequisites are completed. Continuing with scripts install."
 else
     log ERROR "Ending the post-install script."
